ecebe4202cfbf685d1c2946336eef712c36c15f0
Casper <casper770831@gmail.com>
2018-05-26 14:30:35 +0800

    Initial Commit

---
+++ VepIOSAPP/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VepIOSAPP
+//
+//  Created by Casper on 2018/5/26.
+//  Copyright © 2018年 esunergy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VepIOSAPP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VepIOSAPP
+//
+//  Created by Casper on 2018/5/26.
+//  Copyright © 2018年 esunergy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
